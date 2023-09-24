2b4a072433103beaa933beca9307b696b5be481f
Bgbisdbg <dimon123098@bk.ru>
2023-09-24 19:28:37 +0300

    1. Создать модели в проекте Product и ProductCategory (обязательно должно быть поле с изображениями) и выполнить миграции
    2. Поработать с моделями в консоли
    
    3. Создать суперпользователя. Настроить админку и поработать в ней
    
    4. Организовать работу с моделями в контроллерах и html-шаблонах
    
    5. Настроить проект для работы с медиафайлами
    
    6. Создать диспетчер URL-адресов в приложении.
    
    Скорректировать динамические URL-адреса в шаблонах
    
    7. Организовать загрузку данных в базу из файла (Django fixtures)

---
+++ my_store/my_store/__init__.py
@@ -0,0 +1,5 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_store.settings")
+django.setup()
--- my_store/products/models.py
+++ my_store/products/models.py
@@ -5,6 +5,9 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)  # Варчар
     description = models.TextField(null=True, blank=True)  # Текст
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)

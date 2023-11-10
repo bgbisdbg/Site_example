a5afda1f26c78415b2af3a0c72625d06d058bc27
Bgbisdbg <dimon123098@bk.ru>
2023-11-10 15:53:21 +0700

    Перефод функций пользователей в классовое предстовление

--- my_store/users/views.py
+++ my_store/users/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
-from django.urls import reverse
-from users.models import User
+from django.contrib import auth
+from django.urls import reverse, reverse_lazy
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
-from django.contrib.auth.decorators import login_required
+
+from django.views.generic.edit import CreateView, UpdateView
+from users.models import User
+
 
 def login(request):
     if request.method == 'POST':
@@ -22,40 +24,66 @@ def login(request):
     return render(request, 'users/Login.html', context)
 
 
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Поздравляем! Вы успешно зарегистрирв!')
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title'] = 'Store - Регистрация'
+        return context
 
-            return HttpResponseRedirect(reverse('users:login'))
-    else:
-        form = UserRegistrationForm
-    context = {'form': form}
-    return render(request, "users/registration.html", context)
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            print(form.errors)
-    else:
-        form = UserProfileForm(instance=request.user)
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
 
-    context = {
-               'title': 'Store - Профиль',
-               'form': form,
-               'baskets': Basket.objects.filter(user=request.user),
-               }
-    return render(request, 'users/profile.html', context)
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = 'Store - Личный кабинет'
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
 
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Поздравляем! Вы успешно зарегистрирв!')
+#
+#             return HttpResponseRedirect(reverse('users:login'))
+#     else:
+#         form = UserRegistrationForm
+#     context = {'form': form}
+#     return render(request, "users/registration.html", context)
+
+
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('users:profile'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserProfileForm(instance=request.user)
+#
+#     context = {
+#                'title': 'Store - Профиль',
+#                'form': form,
+#                'baskets': Basket.objects.filter(user=request.user),
+#                }
+#     return render(request, 'users/profile.html', context)

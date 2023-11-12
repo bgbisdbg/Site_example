8c64a3079dbb4456fae3efc64115ffdc1e1b7afd
Bgbisdbg <dimon123098@bk.ru>
2023-11-12 19:47:11 +0300

    Добовление БД postgreySQL

--- my_store/my_store/settings.py
+++ my_store/my_store/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'my_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {  # Настройки БД здесь указывае какая база будет использоваться
-    'default': {  # И где она распаложена
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "store_db",
+        "USER": "postgres",
+        "PASSWORD": "qw89op12",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
